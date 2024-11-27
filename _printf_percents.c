b305f9a04d69756702a7d6dcd0b49583dd6f5268
InspectriceColumbo <9949@holbertonstudents.com>
2024-11-27 15:01:11 +0100

    prototype cs percents

---
+++ _printf_percents.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _printf_percents - Prints a percentage character
+ * @args: argument of type va_list (list of args passed to the function)
+ * Return: 1
+ */
+
+int _printf_percents(va_list args)
+{
+	(void)args;
+	_putchar("%");
+        return (1);
+}
