327f137c9993d43586217528508b959fb5a4d275
InspectriceColumbo <9949@holbertonstudents.com>
2024-11-27 23:16:30 +0100

    modifs gcc printf

--- _printf.c
+++ _printf.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * _printf - function that produces output according to a format
+ * @format: string containing the format specifiers and text
+ * @...: variable number of args that correspond to the format specifiers
+ * Return: (count)
+ */
 int _printf(const char *format, ...)
 {
 	int count = 0;
@@ -10,35 +15,36 @@ int _printf(const char *format, ...)
 	for (ptr = format; *ptr != '\0'; ptr++)
 	{
 		if (*ptr == '%' && *(ptr + 1) != '\0')
-		{
 			ptr++;
-			if (*ptr == 'c')
-			{
-				char c = va_arg(args, int);
+		if (*ptr == 'c')
+		{
+			char c = va_arg(args, int);
 
-				_putchar(c);
-				count++;
-			}
-			else if (*ptr == 's')
-			{
-				char *str = va_arg(args, char *);
-
-				for (int i = 0; str[i] != '\0'; i++)
-					_putchar(str[i]);
-				count++;
-			}
-			else if (*ptr == '%')
-				_putchar('%');
+			_putchar(c);
 			count++;
-			else
-				_putchar('%');
-			_putchar(*ptr);
-			count += 2;
 		}
-		else
-			_putchar(*ptr);
+		else if (*ptr == 's')
+		{
+			char *str = va_arg(args, char *);
+			int i;
+
+			for (i = 0; str[i] != '\0'; i++)
+				_putchar(str[i]);
+			count++;
+		}
+		else if (*ptr == '%')
+			_putchar('%');
 		count++;
 	}
-	va_end(args);
-	return (count);
+	else
+		_putchar('%');
+	_putchar(*ptr);
+	count += 2;
+}
+else
+_putchar(*ptr);
+count++;
+}
+va_end(args);
+return (count);
 }
