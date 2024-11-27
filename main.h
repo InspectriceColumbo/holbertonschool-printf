effdef906bf6487d8a515c9f52fb054000cf17e5
InspectriceColumbo <9949@holbertonstudents.com>
2024-11-27 16:15:46 +0100

    main.h betty correction

--- main.h
+++ main.h
@@ -3,19 +3,15 @@
 
 #include <stdlib.h>
 #include <stdarg.h>
-#include <unistd.h> 
+#include <unistd.h>
 #include <limits.h>
 
 /**
-*stdlib : malloc free
-*stdarg : variadic function
-*unistd : write
-*limits : set limits of data types
+* stdlib - malloc free
+* stdarg - variadic function
+* unistd - write
+* limits - set limits of data types
 */
-
-/**
- * function prototypes
- */
 
 int _printf(const char *format, ...);
 int _putchar(char c);
