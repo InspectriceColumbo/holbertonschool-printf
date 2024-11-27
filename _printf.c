7cb5c52b311d9f47726c75c9c963ef4a9d333555
InspectriceColumbo <9949@holbertonstudents.com>
2024-11-27 16:35:01 +0100

    betty printf

--- _printf.c
+++ _printf.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-typedef struct format_specifier {
+typedef struct format_specifier
+{
 	char specifier;
 	int (*func)(va_list);
 }
@@ -15,53 +16,55 @@ format_specifier_t specifiers[] = {
 
 int _printf(const char *format, ...)
 {
-    int count = 0;
-    const char *ptr;
-    va_list args;
+	int count = 0;
+	const char *ptr;
+	va_list args;
 
-    va_start(args, format);  
+	va_start(args, format);
 
-    for (ptr = format; *ptr != '\0'; ptr++)
-      {
-        if (*ptr == '%' && *(ptr + 1) != '\0')
-	  {
-            ptr++; 
-
-           
-            if (*ptr == 'c')
-	      {
-                char c = va_arg(args, int);
-                _putchar(c);
-                count++;
-            } 
-            else if (*ptr == 's')
-	      {
-                char *str = va_arg(args, char *);
-                for (int i = 0; str[i] != '\0'; i++)
-		  {
-                    _putchar(str[i]);
-                    count++;
-                }
-            }
-            else if (*ptr == '%')
-	    {
-	      _putchar('%');
-                count++;
-            } 
-            else
-	    {
-                 _putchar('%');
-                _putchar(*ptr);
-                count += 2;
-            }
-        } 
-        else
+	for (ptr = format; *ptr != '\0'; ptr++)
 	{
-            _putchar(*ptr);
-            count++;
-        }
-    }
+		if (*ptr == '%' && *(ptr + 1) != '\0')
+		{
+			ptr++;
 
-    va_end(args);
-    return count;
+
+			if (*ptr == 'c')
+			{
+				char c = va_arg(args, int);
+
+				_putchar(c);
+				count++;
+			}
+			else if (*ptr == 's')
+			{
+				char *str = va_arg(args, char *);
+
+				for (int i = 0; str[i] != '\0'; i++)
+				{
+					_putchar(str[i]);
+					count++;
+				}
+			}
+			else if (*ptr == '%')
+			{
+				_putchar('%');
+				count++;
+			}
+			else
+			{
+				_putchar('%');
+				_putchar(*ptr);
+				count += 2;
+			}
+		}
+		else
+		{
+			_putchar(*ptr);
+			count++;
+		}
+	}
+
+	va_end(args);
+	return (count);
 }
