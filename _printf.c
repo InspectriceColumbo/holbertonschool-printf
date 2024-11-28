8b539a21d36ea7806332213d09d981290fba0591
Doku-Dow <shems.alilat@outlook.fr>
2024-11-28 12:13:15 +0100

    correction of printf, 3rd try

--- _printf.c
+++ _printf.c
@@ -7,44 +7,54 @@
  */
 int _printf(const char *format, ...)
 {
-	int count = 0;
-	const char *ptr;
-	va_list args;
+    int count = 0;
+    const char *ptr;
+    va_list args;
 
-	va_start(args, format);
-	for (ptr = format; *ptr != '\0'; ptr++)
-	{
-		if (*ptr == '%' && *(ptr + 1) != '\0')
-			ptr++;
-		if (*ptr == 'c')
-		{
-			char c = va_arg(args, int);
+    va_start(args, format);
 
-			_putchar(c);
-			count++;
-		}
-		else if (*ptr == 's')
-		{
-			char *str = va_arg(args, char *);
-			int i;
+    for (ptr = format; *ptr != '\0'; ptr++)
+    {
+        if (*ptr == '%' && *(ptr + 1) != '\0')
+        {
+            ptr++;
+            if (*ptr == 'c')
+            {
+                char c = va_arg(args, int);
+                _putchar(c);
+                count++; 
+            }
+            else if (*ptr == 's')
+            {
+                char *str = va_arg(args, char *);
+                int i = 0;
 
-			for (i = 0; str[i] != '\0'; i++)
-				_putchar(str[i]);
-			count++;
-		}
-		else if (*ptr == '%')
-			_putchar('%');
-		count++;
-	}
-	else
-		_putchar('%');
-	_putchar(*ptr);
-	count += 2;
-}
-else
-_putchar(*ptr);
-count++;
-}
-va_end(args);
-return (count);
+                while (str[i] != '\0')
+                {
+                    _putchar(str[i]);
+                    i++;
+                    count++;
+                }
+            }
+            else if (*ptr == '%')
+            {
+                _putchar('%');
+                count++;  
+            }
+            else
+	      {
+                _putchar('%');
+                _putchar(*ptr);
+                count += 2;  
+            }
+        }
+        else  
+        {
+            _putchar(*ptr);
+            count++;  
+        }
+    }
+
+    va_end(args); 
+    return (count);  
 }
