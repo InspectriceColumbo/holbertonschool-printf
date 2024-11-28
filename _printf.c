14bc392b28077429b857aa0021d463aca0394812
Doku-Dow <shems.alilat@outlook.fr>
2024-11-28 21:32:41 +0100

    betty pas fini

--- _printf.c
+++ _printf.c
@@ -8,40 +8,40 @@
 
 int _printf(const char *format, ...)
 {
-    format_specifier_t specifiers[] = {
-        {'c', _printf_chars}, {'s', _printf_string},
-        {'d', _printf_decimals}, {'i', _printf_integers},
-        {'%', _printf_percents}, {'\0', NULL},
-    };
-    va_list args;
-    int i = 0, j, counter = 0;
+format_specifier_t specifiers[] = {
+  {'c', _printf_chars}, {'s', _printf_string},
+  {'d', _printf_decimals}, {'i', _printf_integers},
+  {'%', _printf_percents}, {'\0', NULL},
+};
+ va_list args;
+ int i = 0, j, counter = 0;
 
-    if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-        return (-1);
+ if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+   return (-1);
 
-    va_start(args, format);
-    while (format[i])
-    {
-        if (format[i] != '%')
-            counter += _putchar(format[i]);
-        else
-        {
-            for (j = 0; specifiers[j].specifier != '\0'; j++)
-                if (format[i + 1] == specifiers[j].specifier)
-                {
-                    counter += specifiers[j].func(args);
-                    i++;
-                    break;
-                }
-            if (!specifiers[j].specifier)
-            {
-                counter += _putchar(format[i]);
-                counter += _putchar(format[i + 1]);
-                i++;
-            }
-        }
-        i++;
-    }
-    va_end(args);
-    return (counter);
+ va_start(args, format);
+ while (format[i])
+   {
+     if (format[i] != '%')
+       counter += _putchar(format[i]);
+     else
+       {
+	 for (j = 0; specifiers[j].specifier != '\0'; j++)
+	   if (format[i + 1] == specifiers[j].specifier)
+	     {
+	       counter += specifiers[j].func(args);
+	       i++;
+	       break;
+	     }
+	  if (!specifiers[j].specifier)
+	    {
+	      counter += _putchar(format[i]);
+	      counter += _putchar(format[i + 1]);
+	      i++;
+	    }
+	}
+     i++;
+   }
+ va_end(args);
+ return (counter);
 }
