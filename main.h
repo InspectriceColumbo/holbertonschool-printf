b01d7e876d24a45d86ac7e11f60f7cd859b96ec4
InspectriceColumbo <9949@holbertonstudents.com>
2024-11-28 16:54:03 +0100

    updated main.h

--- main.h
+++ main.h
@@ -14,22 +14,6 @@
 #include <unistd.h>
 #include <stddef.h>
 
-/**
- * @struct format_specifier: A structure to map format specifiers to..
- * ..their handler functions
- * @specifier: A character representing the format specifier
- * @func: A function pointer pointing to the corresponding handler function..
- * ..that processes the respective specifier.
- */
-
-typedef struct format_specifier
-{
-	char specifier;
-	int (*func)(va_list);
-}
-
-format_specifier_t;
-
 int _printf(const char *format, ...);
 int _putchar(char c);
 int _printf_string(va_list args);
