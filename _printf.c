1c16be0c74106cb6b24247c7cb6cb7aaf0b50014
InspectriceColumbo <9949@holbertonstudents.com>
2024-11-28 12:24:34 +0100

    modifs betty

--- _printf.c
+++ _printf.c
@@ -7,54 +7,55 @@
  */
 int _printf(const char *format, ...)
 {
-    int count = 0;
-    const char *ptr;
-    va_list args;
+	int count = 0;
+	const char *ptr;
+	va_list args;
 
-    va_start(args, format);
+	va_start(args, format);
 
-    for (ptr = format; *ptr != '\0'; ptr++)
-    {
-        if (*ptr == '%' && *(ptr + 1) != '\0')
-        {
-            ptr++;
-            if (*ptr == 'c')
-            {
-                char c = va_arg(args, int);
-                _putchar(c);
-                count++; 
-            }
-            else if (*ptr == 's')
-            {
-                char *str = va_arg(args, char *);
-                int i = 0;
+	for (ptr = format; *ptr != '\0'; ptr++)
+	{
+		if (*ptr == '%' && *(ptr + 1) != '\0')
+		{
+			ptr++;
+			if (*ptr == 'c')
+			{
+				char c = va_arg(args, int);
 
-                while (str[i] != '\0')
-                {
-                    _putchar(str[i]);
-                    i++;
-                    count++;
-                }
-            }
-            else if (*ptr == '%')
-            {
-                _putchar('%');
-                count++;  
-            }
-            else
-	      {
-                _putchar('%');
-                _putchar(*ptr);
-                count += 2;  
-            }
-        }
-        else  
-        {
-            _putchar(*ptr);
-            count++;  
-        }
-    }
+				_putchar(c);
+				count++;
+			}
+			else if (*ptr == 's')
+			{
+				char *str = va_arg(args, char *);
+				int i = 0;
 
-    va_end(args); 
-    return (count);  
+				while (str[i] != '\0')
+				{
+					_putchar(str[i]);
+					i++;
+					count++;
+				}
+			}
+			else if (*ptr == '%')
+			{
+				_putchar('%');
+				count++;
+			}
+			else
+			{
+				_putchar('%');
+				_putchar(*ptr);
+				count += 2;
+			}
+		}
+		else
+		{
+			_putchar(*ptr);
+			count++;
+		}
+	}
+
+	va_end(args);
+	return (count);
 }
